e18359ed7e674d56b61daeeacd058be8694853f5
xXx-Sujan-xXx <134683424+xXx-Sujan-xXx@users.noreply.github.com>
2023-06-09 18:28:02 +0545

    First Commit

---
+++ administrator/views.py
@@ -0,0 +1,322 @@
+from django.shortcuts import render, reverse, redirect
+from account.forms import CustomUserForm
+from voting.forms import *
+from django.contrib import messages
+from django.http import JsonResponse
+from django.conf import settings
+
+
+def find_n_winners(data, n):
+    final_list = []
+    candidate_data = data[:]
+    for i in range(0, n):
+        if len(candidate_data) == 0:
+            continue
+        this_winner = max(candidate_data, key=lambda x: x['votes'])
+        this = this_winner['name'] + \
+               " with " + str(this_winner['votes']) + " votes"
+        final_list.append(this)
+        candidate_data.remove(this_winner)
+    return ", &nbsp;".join(final_list)
+
+
+def dashboard(request):
+    positions = Position.objects.all().order_by('priority')
+    candidates = Candidate.objects.all()
+    voters = Voter.objects.all()
+    voted_voters = Voter.objects.filter(voted=1)
+    chart_data = {}
+
+    for position in positions:
+        list_of_candidates = []
+        votes_count = []
+        for candidate in Candidate.objects.filter(position=position):
+            list_of_candidates.append(candidate.fullname)
+            votes = Votes.objects.filter(candidate=candidate).count()
+            votes_count.append(votes)
+        chart_data[position] = {
+            'candidates': list_of_candidates,
+            'votes': votes_count,
+            'pos_id': position.id
+        }
+
+    context = {
+        'position_count': positions.count(),
+        'candidate_count': candidates.count(),
+        'voters_count': voters.count(),
+        'voted_voters_count': voted_voters.count(),
+        'positions': positions,
+        'chart_data': chart_data,
+        'page_title': "Dashboard"
+    }
+    return render(request, "admin/home.html", context)
+
+
+def voters(request):
+    voters = Voter.objects.all()
+    userForm = CustomUserForm(request.POST or None)
+    voterForm = VoterForm(request.POST or None)
+    context = {
+        'form1': userForm,
+        'form2': voterForm,
+        'voters': voters,
+        'page_title': 'Voters List'
+    }
+    if request.method == 'POST':
+        if userForm.is_valid() and voterForm.is_valid():
+            user = userForm.save(commit=False)
+            voter = voterForm.save(commit=False)
+            voter.admin = user
+            user.save()
+            voter.save()
+            messages.success(request, "New voter created")
+        else:
+            messages.error(request, "Form validation failed")
+    return render(request, "admin/voters.html", context)
+
+
+def view_voter_by_id(request):
+    voter_id = request.GET.get('id', None)
+    voter = Voter.objects.filter(id=voter_id)
+    context = {}
+    if not voter.exists():
+        context['code'] = 404
+    else:
+        context['code'] = 200
+        voter = voter[0]
+        context['first_name'] = voter.admin.first_name
+        context['last_name'] = voter.admin.last_name
+        context['phone'] = voter.phone
+        context['id'] = voter.id
+        context['email'] = voter.admin.email
+    return JsonResponse(context)
+
+
+def view_position_by_id(request):
+    pos_id = request.GET.get('id', None)
+    pos = Position.objects.filter(id=pos_id)
+    context = {}
+    if not pos.exists():
+        context['code'] = 404
+    else:
+        context['code'] = 200
+        pos = pos[0]
+        context['name'] = pos.name
+        context['max_vote'] = pos.max_vote
+        context['id'] = pos.id
+    return JsonResponse(context)
+
+
+def updateVoter(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        instance = Voter.objects.get(id=request.POST.get('id'))
+        user = CustomUserForm(request.POST or None, instance=instance.admin)
+        voter = VoterForm(request.POST or None, instance=instance)
+        user.save()
+        voter.save()
+        messages.success(request, "Voter's bio updated")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('adminViewVoters'))
+
+
+def deleteVoter(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        admin = Voter.objects.get(id=request.POST.get('id')).admin
+        admin.delete()
+        messages.success(request, "Voter Has Been Deleted")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('adminViewVoters'))
+
+
+def viewPositions(request):
+    positions = Position.objects.order_by('-priority').all()
+    form = PositionForm(request.POST or None)
+    context = {
+        'positions': positions,
+        'form1': form,
+        'page_title': "Positions"
+    }
+    if request.method == 'POST':
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.priority = positions.count() + 1  # Just in case it is empty.
+            form.save()
+            messages.success(request, "New Position Created")
+        else:
+            messages.error(request, "Form errors")
+    return render(request, "admin/positions.html", context)
+
+
+def updatePosition(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        instance = Position.objects.get(id=request.POST.get('id'))
+        pos = PositionForm(request.POST or None, instance=instance)
+        pos.save()
+        messages.success(request, "Position has been updated")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('viewPositions'))
+
+
+def deletePosition(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        pos = Position.objects.get(id=request.POST.get('id'))
+        pos.delete()
+        messages.success(request, "Position Has Been Deleted")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('viewPositions'))
+
+
+def viewCandidates(request):
+    candidates = Candidate.objects.all()
+    form = CandidateForm(request.POST or None, request.FILES or None)
+    context = {
+        'candidates': candidates,
+        'form1': form,
+        'page_title': 'Candidates'
+    }
+    if request.method == 'POST':
+        if form.is_valid():
+            form = form.save()
+            messages.success(request, "New Candidate Created")
+        else:
+            messages.error(request, "Form errors")
+    return render(request, "admin/candidates.html", context)
+
+
+def updateCandidate(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        candidate_id = request.POST.get('id')
+        candidate = Candidate.objects.get(id=candidate_id)
+        form = CandidateForm(request.POST or None,
+                             request.FILES or None, instance=candidate)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Candidate Data Updated")
+        else:
+            messages.error(request, "Form has errors")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('viewCandidates'))
+
+
+def deleteCandidate(request):
+    if request.method != 'POST':
+        messages.error(request, "Access Denied")
+    try:
+        pos = Candidate.objects.get(id=request.POST.get('id'))
+        pos.delete()
+        messages.success(request, "Candidate Has Been Deleted")
+    except:
+        messages.error(request, "Access To This Resource Denied")
+
+    return redirect(reverse('viewCandidates'))
+
+
+def view_candidate_by_id(request):
+    candidate_id = request.GET.get('id', None)
+    candidate = Candidate.objects.filter(id=candidate_id)
+    context = {}
+    if not candidate.exists():
+        context['code'] = 404
+    else:
+        candidate = candidate[0]
+        context['code'] = 200
+        context['fullname'] = candidate.fullname
+        previous = CandidateForm(instance=candidate)
+        context['form'] = str(previous.as_p())
+    return JsonResponse(context)
+
+
+def ballot_position(request):
+    context = {
+        'page_title': "Ballot Position"
+    }
+    return render(request, "admin/ballot_position.html", context)
+
+
+def update_ballot_position(request, position_id, up_or_down):
+    try:
+        context = {
+            'error': False
+        }
+        position = Position.objects.get(id=position_id)
+        if up_or_down == 'up':
+            priority = position.priority - 1
+            if priority == 0:
+                context['error'] = True
+                output = "This position is already at the top"
+            else:
+                Position.objects.filter(priority=priority).update(
+                    priority=(priority + 1))
+                position.priority = priority
+                position.save()
+                output = "Moved Up"
+        else:
+            priority = position.priority + 1
+            if priority > Position.objects.all().count():
+                output = "This position is already at the bottom"
+                context['error'] = True
+            else:
+                Position.objects.filter(priority=priority).update(
+                    priority=(priority - 1))
+                position.priority = priority
+                position.save()
+                output = "Moved Down"
+        context['message'] = output
+    except Exception as e:
+        context['message'] = e
+
+    return JsonResponse(context)
+
+
+def ballot_title(request):
+    from urllib.parse import urlparse
+    url = urlparse(request.META['HTTP_REFERER']).path
+    from django.urls import resolve
+    try:
+        redirect_url = resolve(url)
+        title = request.POST.get('title', 'No Name')
+        file = open(settings.ELECTION_TITLE_PATH, 'w')
+        file.write(title)
+        file.close()
+        messages.success(
+            request, "Election title has been changed to " + str(title))
+        return redirect(url)
+    except Exception as e:
+        messages.error(request, e)
+        return redirect("/")
+
+
+def viewVotes(request):
+    votes = Votes.objects.all()
+    context = {
+        'votes': votes,
+        'page_title': 'Votes'
+    }
+    return render(request, "admin/votes.html", context)
+
+
+def resetVote(request):
+    Votes.objects.all().delete()
+    Voter.objects.all().update(voted=False, verified=False, otp=None)
+    messages.success(request, "All votes has been reset")
+    return redirect(reverse('viewVotes'))
---
+++ voting/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('ballot/fetch/', views.fetch_ballot, name='fetch_ballot'),
+    path('dashboard/', views.dashboard, name='voterDashboard'),
+    path('ballot/vote', views.show_ballot, name='show_ballot'),
+    path('ballot/vote/preview', views.preview_vote, name='preview_vote'),
+    path('ballot/vote/submit', views.submit_ballot, name='submit_ballot'),
+]
